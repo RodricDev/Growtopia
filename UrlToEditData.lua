0be42b58312845bdd2759366eb3cc59eea0c4171
RodricDev <66401127+RodricDev@users.noreply.github.com>
2023-09-04 18:39:34 +0800

    Update UrlToEditData.lua

--- UrlToEditData.lua
+++ UrlToEditData.lua
@@ -1,10 +1,6 @@
 Istirahat = true
 JamTidur = {
+    "12:00",
     "01:00"
 }
-DelayTidur = 60
-
-delayht = 130 -- DELAY HARVEST
-delayplant = 110 -- DELAY PLANT
-delaypnb = 200 -- DELAY PNB
-delayplace = 125 -- DELAY PLACE
+DelayTidur = 120
