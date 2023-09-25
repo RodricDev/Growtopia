8b69590d2742588f29b0de0f0fe2627412dcb7cb
RodricDev <66401127+RodricDev@users.noreply.github.com>
2023-09-25 08:16:29 +0800

    Update UrlToEditData.lua

--- UrlToEditData.lua
+++ UrlToEditData.lua
@@ -5,8 +5,5 @@ JamTidur = {
 }
 DelayTidur = 60
 
-hargapack = 2000 -- HARGA PACK
-namapack = "world_lock" -- NAMA PACK
-
 SpecialItem = {1828, 1058, 1094, 1096, 1098, 7058, 10228, 11286} --ITEM YANG INGIN DIAMANKAN KE WORLD, COCOK UNTUK HARVEST FEST!
 minSpec = 50 --BOT AKAN DROP ITEM APABILA ITEM TSB MELEBIHI TARGET minSpec
