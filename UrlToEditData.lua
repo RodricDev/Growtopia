24d91c8d9a3bf88a67c7dc1d14e7c2a3201ff8db
RodricDev <66401127+RodricDev@users.noreply.github.com>
2023-12-01 20:55:02 +0800

    Update UrlToEditData.lua

--- UrlToEditData.lua
+++ UrlToEditData.lua
@@ -1,1 +1,6 @@
-nextrandom = true --UBAH JADI FALSE JIKA GAMAU JOIN RANDOM WORLD SEBELUM NEXT WORLD
+--============= [ Pack Settings ] =============--
+packList = {242} -- List of pack id
+packName = "world_lock" -- Pack name in store
+minimumGem = 10000 -- Minimum gems to buy pack
+packPrice = 2000 -- Pack price
+packLimit = 200 -- Limit of buying pack before bp full
