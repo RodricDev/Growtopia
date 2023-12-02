61c3977e6869aee7be87f60e2fac6f9f5ed1fd8a
RodricDev <66401127+RodricDev@users.noreply.github.com>
2023-12-03 00:11:10 +0800

    Update UrlToEditData.lua

--- UrlToEditData.lua
+++ UrlToEditData.lua
@@ -1,6 +1,6 @@
 --============= [ Pack Settings ] =============--
-packList = {14138} -- List of pack id
-packName = "winterfest_calendar_2023" -- Pack name in store
-minimumGem = 65000 -- Minimum gems to buy pack
-packPrice = 65000 -- Pack price
+packList = {242} -- List of pack id
+packName = "world_lock" -- Pack name in store
+minimumGem = 10000 -- Minimum gems to buy pack
+packPrice = 2000 -- Pack price
 packLimit = 200 -- Limit of buying pack before bp full
