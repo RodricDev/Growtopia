0d54d495dfadfc9c73c4efcd1100e717364e1754
RodricDev <66401127+RodricDev@users.noreply.github.com>
2023-12-04 00:07:58 +0800

    Update UrlToEditData.lua

--- UrlToEditData.lua
+++ UrlToEditData.lua
@@ -4,3 +4,5 @@ packName = "world_lock" -- Pack name in store
 minimumGem = 10000 -- Minimum gems to buy pack
 packPrice = 2000 -- Pack price
 packLimit = 200 -- Limit of buying pack before bp full
+
+nextrandom = true
