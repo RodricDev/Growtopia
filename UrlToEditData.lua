dfca1e4b8b62acb581f0ee855c5760b3d4d05145
RodricDev <66401127+RodricDev@users.noreply.github.com>
2023-12-05 10:11:13 +0800

    Update UrlToEditData.lua

--- UrlToEditData.lua
+++ UrlToEditData.lua
@@ -1,12 +1,1 @@
---============= [ Pack Settings ] =============--
-packList = {242} -- List of pack id
-packName = "world_lock" -- Pack name in store
-minimumGem = 10000 -- Minimum gems to buy pack
-packPrice = 2000 -- Pack price
-packLimit = 200 -- Limit of buying pack before bp full
-
 nextrandom = true
-delayHarvest = 120 -- Harvesting delay
-delayPlant = 100 -- Planting delay
-delayPunch = 160 -- Punching delay
-delayPlace = 110 -- Placing delay
