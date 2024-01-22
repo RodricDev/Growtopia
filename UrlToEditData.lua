90327292041988810f8fe8376f6770ec58b19736
RodricDev <66401127+RodricDev@users.noreply.github.com>
2024-01-23 00:42:46 +0800

    Update UrlToEditData.lua

--- UrlToEditData.lua
+++ UrlToEditData.lua
@@ -1,12 +1,1 @@
 nextrandom = false
----===== BASIC SETTINGS =====---
-target_block = 185 -- TARGET MAX HARVEST (JML BLOCK DI TAS)
-target_seed = 1 -- TARGET MINIMAL SEED UNTUK DITANAM
-delayht = 125 -- DELAY HARVEST
-delayplant = 100 -- DELAY PLANT
-delaypnb = 180 -- DELAY PNB
-delayplace = 125 -- DELAY PLACE
-delayworld = 10000 -- DELAY MASUK DOOR
-delaydrop = 1000 -- JEDA DROP PER ITEM
-delaytrash = 100
-targettrash = 100 -- AUTO TRASH JUNK ITEMS KETIKA MENCUKUPI TARGET
