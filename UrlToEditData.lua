5a0f6e176dd8045169821fe8d877bfadd6e36446
RodricDev <66401127+RodricDev@users.noreply.github.com>
2024-02-14 16:29:39 +0800

    Update UrlToEditData.lua

--- UrlToEditData.lua
+++ UrlToEditData.lua
@@ -1,1 +1,5 @@
-
+hargapack = 7500 -- HARGA PACK
+namapack = "valentines" -- NAMA PACK
+MinItemPack = 10 --APABILA JUMLAH ITEM YANG ADA DIDALAM 
+maxbuy = 5 --MAKSIMAL BOTNYA AKAN REPEAT BELI PACK JIKA 
+iditempack = {3402}
