9cb6f66dc53607fc15dcb41ac9fbbfe981e9cb45
RodricDev <66401127+RodricDev@users.noreply.github.com>
2024-02-19 11:12:15 +0800

    Update UrlToEditData.lua

--- UrlToEditData.lua
+++ UrlToEditData.lua
@@ -1,6 +1,1 @@
-hargapack = 7500 -- HARGA PACK
-namapack = "valentines" -- NAMA PACK
-MinItemPack = 10 --APABILA JUMLAH ITEM YANG ADA DIDALAM 
-maxbuy = 5 --MAKSIMAL BOTNYA AKAN REPEAT BELI PACK JIKA 
-iditempack = {3402}
-DelayRecon = 30000 --JEDA RECONNECT (Ms)
+
