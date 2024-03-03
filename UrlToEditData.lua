7b7fe13ac3f4850af6cba9df8c203eb73d5b6af1
RodricDev <66401127+RodricDev@users.noreply.github.com>
2024-03-03 16:57:58 +0800

    Update UrlToEditData.lua

--- UrlToEditData.lua
+++ UrlToEditData.lua
@@ -1,6 +1,1 @@
--- EVENT SETTINGS
-SpecialItem = {5036,7164}       -- Put all your id item special in here
-minSpec = 100                   -- Bot will dropping the item when he have X minSpec or more
-WorldSpec = "RICSAMPAH6651"     -- World Special Item
-ID_AcuanDrop_Spec = 20          -- ID Foreground or Background for Drop Special Item
-WorldSeed = {"BUKOEPPRSTRB","BUKOEPPRSTRC","BUKOEPPRSTRD","BUKOEPPRSTRE","BUKOEPPRSTRF"} -- Put your World Seed here
+
