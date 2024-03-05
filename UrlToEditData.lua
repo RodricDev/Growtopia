b88d35e49b49f598577693fcc502537c6ee40e00
RodricDev <66401127+RodricDev@users.noreply.github.com>
2024-03-06 00:23:28 +0800

    Update UrlToEditData.lua

--- UrlToEditData.lua
+++ UrlToEditData.lua
@@ -1,6 +1,1 @@
----===== Bot Off =====---
-Istirahat = true --JADIIN FALSE JIKA GAMAU ISTIRAHATIN BOTNYA
-JamTidur = {
-    "14:00"
-} -- JAM DAN MENIT BOTNYA OFF (Format 24 Jam)
-DelayTidur = 60 -- BOTNYA OFF SELAMA n DelayTidur (Format Menit)
+
