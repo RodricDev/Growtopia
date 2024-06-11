99f380cb40a42f685d883f411746276176cddbe8
RodricDev <66401127+RodricDev@users.noreply.github.com>
2024-06-12 02:18:00 +0800

    Update UrlToEditData.lua

--- UrlToEditData.lua
+++ UrlToEditData.lua
@@ -1,6 +1,1 @@
-TimeRelogBotMatung = 10
-hargapack = 2000 -- HARGA PACK
-namapack = "world_lock" -- NAMA PACK
-MinItemPack = 5 --APABILA JUMLAH ITEM YANG ADA DIDALAM 
-maxbuy = 5 --MAKSIMAL BOTNYA AKAN REPEAT BELI PACK JIKA 
-iditempack = {242}
+
